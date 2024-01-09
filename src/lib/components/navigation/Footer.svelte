a1e9df586bddf244c73af2ab71a6dba92b86cb16
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2024-01-09 11:04:21 -0700

    add dynamic year to footer

--- src/lib/components/navigation/Footer.svelte
+++ src/lib/components/navigation/Footer.svelte
@@ -2,6 +2,8 @@
 	//Footer component with offset to compensate for whole page offset 
 	import { topValue } from '$lib/store';
 
+	const currentYear: number = new Date().getFullYear();
+
 </script>
 
 <footer aria-label="footer" class="mt-auto border-t-4 border-darkblue bg-offwhite relative" style="top: {$topValue}px;">
@@ -9,7 +11,7 @@
 		<div class="flex flex-col md:flex-row items-center justify-between text-darkblue">
 			<a href="/" class="font-display text-xl md:text-3xl">The Paint Atheneum</a>
 			<p class="mt-4 text-center text lg:mt-0 lg:text-right font-serif">
-				Copyright &copy; 2022. All rights reserved.
+				Copyright &copy; {currentYear}. All rights reserved.
 			</p>
 		</div>
 	</div>
